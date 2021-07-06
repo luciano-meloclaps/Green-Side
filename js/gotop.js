2219c9d6be95ddc479bc885f9bc9d588fb5de005
Luciano Melo Claps <meloclapsluciano@gmail.com>
2021-07-05 23:36:36 -0300

    commit inicial

---
+++ js/gotop.js
@@ -0,0 +1,17 @@
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 80){
+        document.querySelector('.go-top-container')
+        .classList.add('show');
+    }else{
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+    }
+}   
+
+document.querySelector('.go-top-container')
+.addEventListener('click', () =>{
+    window.scrollTop({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
