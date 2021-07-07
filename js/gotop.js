9f11f0328bba3ca4a3e064d2b8fd6f291e85ba64
Luciano Melo Claps <meloclapsluciano@gmail.com>
2021-07-07 19:25:48 -0300

    añadi nuevos banners y modifique el color del body de la pagina

--- js/gotop.js
+++ js/gotop.js
@@ -1,17 +1,6 @@
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 80){
-        document.querySelector('.go-top-container')
-        .classList.add('show');
-    }else{
-        document.querySelector('.go-top-container')
-        .classList.remove('show');
-    }
-}   
-
-document.querySelector('.go-top-container')
-.addEventListener('click', () =>{
-    window.scrollTop({
-        top: 0,
-        behavior: 'smooth',
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
-});
+  });
