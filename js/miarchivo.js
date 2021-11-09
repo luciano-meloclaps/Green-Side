b8be3128ba9f984a69cb1fcdddb405d4cd094727
luciano-meloclaps <meloclapsluciano@gmail.com>
2021-11-09 18:43:26 -0300

    haciendo el footer del carrito

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,10 +1,10 @@
-// Cards
-// const cards = document.getElementById('cards')
+// Cards, Footer y Carrito templates
 const items = document.getElementById('items')
-// const footer = document.getElementById('footer')
+const cards = document.getElementById('cards')
+const footer = document.getElementById('footer')
 const templateCard = document.getElementById('template-card').content
-// const templateFooter = document.getElementById('template-footer').content
-// const templateCarrito = document.getElementById('template-carrito').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment()
 
 //Crear carrito
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     fetchData()
 });
 
-items.addEventListener('click', e => {
+cards.addEventListener('click', e => {
     addCarrito(e)
 })
 
@@ -32,7 +32,6 @@ const fetchData = async () => {
 
 // Pintar productos
 const pintarCards = data => {
-    console.log(data)
     data.forEach(producto => {
         templateCard.querySelector('h5').textContent = producto.producto
         templateCard.querySelector('p').textContent = producto.precio
@@ -42,7 +41,7 @@ const pintarCards = data => {
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     })
-        items.appendChild(fragment)
+        cards.appendChild(fragment)
 }
 
 //Ejecutamos evento y mandamos a setCarrito
@@ -56,7 +55,7 @@ const addCarrito = e => {
     
 }
 
-//Capturar elementos
+//Capturar elementos que van a ser empujados en el carrito
 const setCarrito = objeto => {
     const producto = {
         //Accedo a la informacion
@@ -73,7 +72,55 @@ const setCarrito = objeto => {
 
     //Empujar al carrito 
     carrito[producto.id] = {...producto}
-    
-    console.log(carrito)
+    pintarCarrito()
+
 }
 
+//Recorrer e imprimir el carrito con los elementos capturados anteriormente
+const pintarCarrito = () => {
+    // console.log(carrito)
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+
+        //botones
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+
+        //Clonar
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+
+    //Pintamos
+    items.appendChild(fragment)
+
+    pintarFooter()
+
+}
+
+//Mostar el mensaje del footer
+const pintarFooter = () => {
+    footer.innerHTML = ""  
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+            <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th> `
+    }
+
+    //Funcion flecha y retorno (acc > acumulado)
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
+    // console.log(nPrecio)
+
+    //Imprimir
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+
+    footer.appendChild(fragment)
+}           
